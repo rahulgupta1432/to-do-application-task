13d7326b29f6b422c4eacea2c7da55ba96da335b
rahulgupta1432 <rahulkumar058790@gmail.com>
2024-06-08 01:04:36 +0530

    update db config with mysql2 dialect

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 
 const seuqelizeCon=new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
     host:process.env.DB_HOST,
-    dialect:'mysql'
+    dialect:'mysql',
+    dialectModule: require("mysql2")
 });
 
 
